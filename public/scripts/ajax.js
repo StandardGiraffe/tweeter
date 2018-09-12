110a81832882e8b18857ceb907c13904592a663f
StandardGiraffe <dannyfekete@gmail.com>
2018-09-12 17:45:24 +0000

    Linked ajax.js to the page and overrode the submit button to print a test string and not send a post request to the server.

---
+++ public/scripts/ajax.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $(".new-tweet")
+  .submit(function(event) {
+    event.preventDefault();
+    alert("You clicked me, you magnificent bastard!");
+  });
+
+
+
+
+});
